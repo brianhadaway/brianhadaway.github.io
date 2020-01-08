f1ee9234a9935b020a46a6065ec62389f637330e
Brian Hadaway <brianhadaway@gmail.com>
2020-01-08 09:55:26 -0800

    Updates

---
+++ precache-manifest.a87c990c24244f2107f76c22e27612d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35e75ee9987184a5f93b9d46b4dd2bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d04de854f213bae55d5e",
+    "url": "/static/css/main.b9000715.chunk.css"
+  },
+  {
+    "revision": "5208fe416036f89efbca",
+    "url": "/static/js/2.75097680.chunk.js"
+  },
+  {
+    "revision": "d04de854f213bae55d5e",
+    "url": "/static/js/main.80c09199.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
